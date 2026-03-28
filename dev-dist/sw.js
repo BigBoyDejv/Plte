8a0e8cf8708750c6d28cf6664b22de677e033b54
BigBoyDejv <jan.rusin3038@gmail.com>
2026-03-28 10:47:26 +0100

    idk

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-1fbd7631'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.726d1q8uqao"
+    "revision": "0.5b2hr1b70ts"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
